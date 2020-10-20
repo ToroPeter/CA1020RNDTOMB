b240fd49f71f9b2dd4d9c9ba48640f47c780c584
ToroPeter <71694444+ToroPeter@users.noreply.github.com>
2020-10-20 18:08:52 +0200

    Add files via upload

---
+++ CA1020RNDTOMB/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1020RNDTOMB
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            int[] tomb = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                tomb[i] = rnd.Next(10, 100);
+                Console.WriteLine(tomb[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
